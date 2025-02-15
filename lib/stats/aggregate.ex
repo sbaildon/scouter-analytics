15b3ee9e77955b9817f1a254c0433095665ace38
Sean Baildon <sean@baildon.co>
2025-02-15 09:20:06 +0000

    pattern match binary literal

--- lib/stats/aggregate.ex
+++ lib/stats/aggregate.ex
@@ -26,43 +26,43 @@ defmodule Stats.Aggregate do
     field :browser_version, :string
   end
 
-  def hash(%{grouping_id: 131_071, site_id: field}), do: do_hash(field)
-  def hash(%{grouping_id: 196_607, timestamp: field}), do: do_hash(field)
-  def hash(%{grouping_id: 229_375, host: field}), do: do_hash(field)
-  def hash(%{grouping_id: 245_759, path: field}), do: do_hash(field)
-  def hash(%{grouping_id: 253_951, referrer: field}), do: do_hash(field)
-  def hash(%{grouping_id: 258_047, utm_medium: field}), do: do_hash(field)
-  def hash(%{grouping_id: 260_095, utm_source: field}), do: do_hash(field)
-  def hash(%{grouping_id: 261_119, utm_campaign: field}), do: do_hash(field)
-  def hash(%{grouping_id: 261_631, utm_content: field}), do: do_hash(field)
-  def hash(%{grouping_id: 261_887, utm_term: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_015, country_code: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_079, subdivision1_code: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_111, subdivision2_code: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_127, city_geoname_id: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_135, operating_system: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_139, operating_system_version: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_141, browser: field}), do: do_hash(field)
-  def hash(%{grouping_id: 262_142, browser_version: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b011111111111111111, site_id: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b101111111111111111, timestamp: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b110111111111111111, host: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111011111111111111, path: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111101111111111111, referrer: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111110111111111111, utm_medium: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111011111111111, utm_source: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111101111111111, utm_campaign: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111110111111111, utm_content: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111011111111, utm_term: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111101111111, country_code: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111110111111, subdivision1_code: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111111011111, subdivision2_code: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111111101111, city_geoname_id: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111111110111, operating_system: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111111111011, operating_system_version: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111111111101, browser: field}), do: do_hash(field)
+  def hash(%{grouping_id: 0b111111111111111110, browser_version: field}), do: do_hash(field)
 
   defp do_hash(term), do: :erlang.phash2(term)
 
-  def field(%{grouping_id: 131_071}), do: :site_id
-  def field(%{grouping_id: 196_607}), do: :timestamp
-  def field(%{grouping_id: 229_375}), do: :host
-  def field(%{grouping_id: 245_759}), do: :path
-  def field(%{grouping_id: 253_951}), do: :referrer
-  def field(%{grouping_id: 258_047}), do: :utm_medium
-  def field(%{grouping_id: 260_095}), do: :utm_source
-  def field(%{grouping_id: 261_119}), do: :utm_campaign
-  def field(%{grouping_id: 261_631}), do: :utm_content
-  def field(%{grouping_id: 261_887}), do: :utm_term
-  def field(%{grouping_id: 262_015}), do: :country_code
-  def field(%{grouping_id: 262_079}), do: :subdivision1_code
-  def field(%{grouping_id: 262_111}), do: :subdivision2_code
-  def field(%{grouping_id: 262_127}), do: :subdivision2_code
-  def field(%{grouping_id: 262_135}), do: :operating_system
-  def field(%{grouping_id: 262_139}), do: :operating_system_version
-  def field(%{grouping_id: 262_141}), do: :browser
-  def field(%{grouping_id: 262_142}), do: :browser_version
+  def field(%{grouping_id: 0b011111111111111111}), do: :site_id
+  def field(%{grouping_id: 0b101111111111111111}), do: :timestamp
+  def field(%{grouping_id: 0b110111111111111111}), do: :host
+  def field(%{grouping_id: 0b111011111111111111}), do: :path
+  def field(%{grouping_id: 0b111101111111111111}), do: :referrer
+  def field(%{grouping_id: 0b111110111111111111}), do: :utm_medium
+  def field(%{grouping_id: 0b111111011111111111}), do: :utm_source
+  def field(%{grouping_id: 0b111111101111111111}), do: :utm_campaign
+  def field(%{grouping_id: 0b111111110111111111}), do: :utm_content
+  def field(%{grouping_id: 0b111111111011111111}), do: :utm_term
+  def field(%{grouping_id: 0b111111111101111111}), do: :country_code
+  def field(%{grouping_id: 0b111111111110111111}), do: :subdivision1_code
+  def field(%{grouping_id: 0b111111111111011111}), do: :subdivision2_code
+  def field(%{grouping_id: 0b111111111111101111}), do: :subdivision2_code
+  def field(%{grouping_id: 0b111111111111110111}), do: :operating_system
+  def field(%{grouping_id: 0b111111111111111011}), do: :operating_system_version
+  def field(%{grouping_id: 0b111111111111111101}), do: :browser
+  def field(%{grouping_id: 0b111111111111111110}), do: :browser_version
 end
