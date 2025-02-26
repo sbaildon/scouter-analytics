c5c04c063eacc274822e68e8e0b5be51f16d4584
Sean Baildon <sean@baildon.co>
2025-02-26 16:31:56 +0000

    fix warnings

--- lib/stats/events_repo.ex
+++ lib/stats/events_repo.ex
@@ -10,14 +10,14 @@ defmodule Stats.EventsRepo do
   def arrow_query(query, opts \\ []) do
     repo = get_dynamic_repo()
 
-    {adapter_meta, opts} = tuplet = Ecto.Repo.Supervisor.tuplet(repo, prepare_opts(:all, opts))
+    {adapter_meta, opts} = Ecto.Repo.Supervisor.tuplet(repo, prepare_opts(:all, opts))
 
     %{adapter: adapter, cache: cache} = adapter_meta
 
     {query, opts} = prepare_query(:all, query, opts)
     query = Planner.attach_prefix(query, opts)
 
-    {query_meta, prepared, cast_params, dump_params} =
+    {query_meta, prepared, _cast_params, dump_params} =
       Planner.query(query, :all, cache, adapter, 0)
 
     {_num, columns} = adapter.execute(adapter_meta, query_meta, prepared, dump_params, opts)
@@ -27,17 +27,18 @@ defmodule Stats.EventsRepo do
 
   def arrow_stream(queryable, opts \\ []) do
     repo = get_dynamic_repo()
-    {adapter_meta, opts} = tuplet = Ecto.Repo.Supervisor.tuplet(repo, prepare_opts(:stream, opts))
+    {adapter_meta, opts} = Ecto.Repo.Supervisor.tuplet(repo, prepare_opts(:stream, opts))
 
     %{adapter: adapter, cache: cache} = adapter_meta
 
     {query, opts} = prepare_query(:stream, queryable, opts)
     query = Planner.attach_prefix(query, opts)
 
-    {query_meta, prepared, cast_params, dump_params} =
+    {query_meta, prepared, _cast_params, dump_params} =
       Planner.query(query, :all, cache, adapter, 0)
 
     adapter.arrow_stream(adapter_meta, query_meta, prepared, dump_params, opts)
+  end
 
   def merge_columns(chunked_results) do
     Stream.zip_with(chunked_results, fn columns ->
