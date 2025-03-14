bac651bd638278ca809d8d87b7df0d66cd904c0e
Sean Baildon <sean@baildon.co>
2025-03-14 11:13:44 +0000

    count: don't fallback if sendBeacon fails

--- priv/static/js/count.js
+++ priv/static/js/count.js
@@ -184,25 +184,7 @@
 		var url = goatcounter.url(vars);
 		if (!url) return warn("not counting because path callback returned null");
 
-		if (!navigator.sendBeacon(url)) {
-			// This mostly fails due to being blocked by CSP; try again with an
-			// image-based fallback.
-			var img = document.createElement("img");
-			img.src = url;
-			img.style.position = "absolute"; // Affect layout less.
-			img.style.bottom = "0px";
-			img.style.width = "1px";
-			img.style.height = "1px";
-			img.loading = "eager";
-			img.setAttribute("alt", "");
-			img.setAttribute("aria-hidden", "true");
-
-			var rm = function () {
-				if (img && img.parentNode) img.parentNode.removeChild(img);
-			};
-			img.addEventListener("load", rm, false);
-			document.body.appendChild(img);
-		}
+		navigator.sendBeacon(url);
 	};
 
 	// Get a query parameter.
