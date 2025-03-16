49e65ea795775dcae49ee03e98c011c508710468
Sean Baildon <sean@baildon.co>
2025-03-16 11:47:20 +0000

    rename domains to sites

---
+++ priv/repo/migrations/20250208105535_create_sites.exs
@@ -0,0 +1,10 @@
+defmodule Stats.Repo.Migrations.CreateSites do
+  use Ecto.Migration
+
+  def change do
+    create table("sites", options: "STRICT") do
+      add :published, :boolean, null: false, default: false
+      timestamps()
+    end
+  end
+end
