183e927260bfad77eeb117af9e6aa53b35520f7b
Sean Baildon <sean@baildon.co>
2025-03-23 19:23:18 +0000

    fmt

--- config/prod.exs
+++ config/prod.exs
@@ -2,11 +2,11 @@ import Config
 
 config :logger, level: :info
 
-config :stats, Telemetry.Endpoint, server: true
-
 config :stats, Dashboard.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
+
+config :stats, Telemetry.Endpoint, server: true
 
 config :swoosh, api_client: Swoosh.ApiClient.Req
 config :swoosh, local: false
