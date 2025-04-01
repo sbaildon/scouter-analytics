253463be101ed4377109541787f6efd343647f43
Sean Baildon <sean@baildon.co>
2025-04-01 10:35:29 +0100

    environment can be trusted | untrusted | unspecified

--- lib/dashboard/plugs/trusted_proxies_plug.ex
+++ lib/dashboard/plugs/trusted_proxies_plug.ex
@@ -5,26 +5,28 @@ defmodule Dashboard.TrustedProxiesPlug do
 
   require Logger
 
-  defmacro is_ipv4_mapped_ipv6("::ffff:" <> _), do: true
-  defmacro is_ipv4_mapped_ipv6(_any), do: false
-
   def init(opts), do: Map.new(opts)
 
   def call(conn, _opts) do
-    {:ok, remote_ip} = remote_ip(conn)
+    if trusted_proxies = trusted_proxies() do
+      {:ok, remote_ip} = remote_ip(conn)
 
-    trusted_environment? =
-      Enum.any?(trusted_proxies(), fn trusted ->
-        remote_ip <= trusted.last && remote_ip >= trusted.first
-      end)
+      trusted_environment? =
+        Enum.any?(trusted_proxies, fn trusted ->
+          remote_ip <= trusted.last && remote_ip >= trusted.first
+        end)
 
-    (trusted_environment? && trusted_environment(conn)) || untrusted_environment(conn)
+      (trusted_environment? && trusted_environment(conn)) || untrusted_environment(conn)
+    else
+      unspecified_environment(conn)
+    end
   end
 
-  defp trusted_environment(conn), do: assign(conn, :trusted_environment?, true)
-  defp untrusted_environment(conn), do: assign(conn, :trusted_environment?, false)
+  defp trusted_environment(conn), do: assign(conn, :environment, :trusted)
+  defp untrusted_environment(conn), do: assign(conn, :environment, :untrusted)
+  defp unspecified_environment(conn), do: assign(conn, :environment, :unspecified)
 
-  defp remote_ip(conn) do
+  defp client_ip(conn) do
     {:ok,
      conn
      |> Map.fetch!(:remote_ip)
@@ -39,23 +41,31 @@ defmodule Dashboard.TrustedProxiesPlug do
   defp ipv4_mapped_prefix, do: "::ffff:"
 
   defp trusted_proxies do
-    trusted_proxies =
-      case Dashboard.Endpoint.config(:trusted_proxies) do
-        nil -> []
-        config when is_binary(config) -> String.split(config, ";")
-      end
+    case Dashboard.Endpoint.config(:trusted_proxies) do
+      nil ->
+        nil
 
-    Enum.map(trusted_proxies, &parse_cidr/1)
+      networks when is_binary(networks) ->
+        networks
+        |> String.split(";")
+        |> Enum.map(&parse_cidr!/1)
+    end
   end
 
-  def parse_cidr(network) when is_ipv4_mapped_ipv6(network), do: CIDR.parse(network)
-  def parse_cidr(network), do: network |> ipv4_network_to_ipv6() |> CIDR.parse()
+  def parse_cidr!("::ffff:" <> _ = network) do
+    case CIDR.parse(network) do
+      {:error, reason} -> raise "#{inspect(reason)}"
+      cidr -> cidr
+    end
+  end
 
-  defp ipv4_network_to_ipv6(ipv4_network) do
-    [ip, mask] = String.split(ipv4_network, "/")
+  def parse_cidr!(network), do: network |> ipv4_network_to_ipv6() |> parse_cidr!()
+
+  defp ipv4_network_to_ipv6(network)  do
+    [ip, mask] = String.split(network, "/")
 
     ipv6_mask = String.to_integer(mask) + 96
 
-    "#{ipv4_mapped_prefix()}#{ip}/#{ipv6_mask}"
+     "#{ipv4_mapped_prefix()}#{ip}/#{ipv6_mask}"
   end
 end
