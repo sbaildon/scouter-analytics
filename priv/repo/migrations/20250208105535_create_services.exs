6f0fd23e20f3534293d32a26c507151cb29354f3
Sean Baildon <sean@baildon.co>
2025-04-12 07:17:38 -0500

    fix service migration

--- priv/repo/migrations/20250208105535_create_services.exs
+++ priv/repo/migrations/20250208105535_create_services.exs
@@ -1,10 +1,14 @@
-defmodule Stats.Repo.Migrations.CreateSites do
+defmodule Stats.Repo.Migrations.CreateServices do
   use Ecto.Migration
 
   def change do
-    create table("sites", options: "STRICT") do
+    create table(:services, options: "STRICT") do
+      add :name, :text, null: false
       add :published, :boolean, null: false, default: true
+
       timestamps()
     end
+
+    create unique_index(:services, :name)
   end
 end
