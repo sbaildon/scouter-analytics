b549b85ce17b697b38f884e10f56284426e44610
Sean Baildon <sean@baildon.co>
2025-04-19 10:32:36 -0500

    build plain authorization header if unspecified envrionment

--- analytics/lib/dashboard/plugs/authz_plug.ex
+++ analytics/lib/dashboard/plugs/authz_plug.ex
@@ -5,6 +5,23 @@ defmodule Dashboard.AuthzPlug do
   def init(opts), do: Map.new(opts)
 
   def call(conn, _opts) do
+    case Map.get(conn.assigns, :environment) do
+      :trusted -> authorize_if_header_present(conn)
+      :untrusted -> raise "untrusted environment"
+      :unspecified -> conn |> build_authorization_header() |> authorize_if_header_present()
+    end
+  end
+
+  defp build_authorization_header(conn) do
+    {:ok, services} = Scouter.Services.list()
+
+    service_ids =
+      Enum.map_join(services, ";", fn service -> service.id end)
+
+    put_req_header(conn, "authorization", "Plain-Text #{service_ids}")
+  end
+
+  defp authorize_if_header_present(conn) do
     case List.keyfind(conn.req_headers, "authorization", 0) do
       {_, authorization} -> authorize_session(conn, authorization)
       nil -> deauthorize_session(conn)
