d756c9ccb4b721b7d5092bf90e88a539661d92e7
Sean Baildon <sean@baildon.co>
2025-04-21 07:55:14 -0500

    add some sane defaults
    attempting to make self-hosting less overwhelming

--- analytics/config/runtime.exs
+++ analytics/config/runtime.exs
@@ -64,7 +64,7 @@ config :scouter, Dashboard.Endpoint,
   url: [host: env!.("DASHBOARD_HOST"), port: 443, scheme: "https"],
   http: [
     ip: {0, 0, 0, 0, 0, 0, 0, 0},
-    port: env_as!.("DASHBOARD_PORT", :integer)
+    port: env_as.("DASHBOARD_PORT", "4000", :integer)
   ],
   secret_key_base: env!.("DASHBOARD_SECRET_KEY_BASE"),
   live_view: [signing_salt: env!.("DASHBOARD_SIGNING_SALT")],
@@ -78,6 +78,11 @@ config :scouter, Oban,
      ]}
   ]
 
+config :scouter, Scouter.EventsRepo,
+  database: env.("EVENT_DATABASE_PATH", "/var/lib/scouter/events.duckdb"),
+  httpfs_credentials: httpfs_credentials.(),
+  pool_size: 1
+
 # config :scouter, Objex,
 #   access_key_id: env!.("AWS_ACCESS_KEY_ID"),
 #   secret_access_key: env!.("AWS_SECRET_KEY"),
@@ -87,23 +92,18 @@ config :scouter, Oban,
 #   region: env.("AWS_REGION", "auto"),
 #   http_client: {Finch, name: Scouter.Finch}
 
-config :scouter, Scouter.EventsRepo,
-  database: env!.("EVENT_DATABASE_PATH"),
-  httpfs_credentials: httpfs_credentials.(),
-  pool_size: 1
-
 config :scouter, Scouter.Geo,
   database: env.("MMDB_PATH", nil)
 
 config :scouter, Scouter.Repo,
-  database: env!.("DATABASE_PATH"),
-  pool_size: env_as.("POOL_SIZE", "10", :integer)
+  database: env.("DATABASE_PATH", "/var/lib/scouter/service.db"),
+  pool_size: env_as.("POOL_SIZE", "1", :integer)
 
 config :scouter, Telemetry.Endpoint,
   url: [host: env!.("TELEMETRY_HOST"), port: 443, scheme: "https"],
   http: [
     ip: {0, 0, 0, 0, 0, 0, 0, 0},
-    port: env_as!.("TELEMETRY_PORT", :integer)
+    port: env_as.("TELEMETRY_PORT", "4001", :integer)
   ],
   secret_key_base: env!.("TELEMETRY_SECRET_KEY_BASE")
 
