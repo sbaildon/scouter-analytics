cba408efe821dea112bc4cc09e64df95996597c2
Sean Baildon <sean@baildon.co>
2025-07-09 08:51:25 -0500

    only care about "from" if it's an email address

--- lib/dashboard/plugs/from_plug.ex
+++ lib/dashboard/plugs/from_plug.ex
@@ -17,13 +17,25 @@ defmodule Dashboard.FromPlug do
   end
 
   defp maybe_assign_from(conn) do
-    case List.keyfind(conn.req_headers, "from", 0) do
-      {_, from} ->
-        Logger.debug(user: from)
-        conn |> put_session(:from, from) |> assign(:from, from)
+    with {_, from} <- List.keyfind(conn.req_headers, "from", 0),
+         {:ok, email} <- parse_email(from) do
+      Logger.debug(user: from)
+      conn |> put_session(:from, email) |> assign(:from, email)
+    else
+      _ -> conn
+    end
+  end
 
-      nil ->
-        conn
+  defp parse_email(potential_email) do
+    types = %{email: :string}
+
+    {%{}, types}
+    |> Ecto.Changeset.cast(%{"email" => potential_email}, Map.keys(types))
+    |> EctoHelpers.validate_email()
+    |> Ecto.Changeset.apply_action(:validate)
+    |> case do
+      {:ok, %{email: email}} -> {:ok, email}
+      other -> other
     end
   end
 end
