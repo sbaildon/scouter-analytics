ae677c11888fd75a1a4ff0cf7d9094a8c71efb13
Sean Baildon <sean@baildon.co>
2025-11-15 22:33:31 -0500

    main instance is conditional

--- lib/scouter/instances.ex
+++ lib/scouter/instances.ex
@@ -15,10 +15,25 @@ defmodule Scouter.Instances do
     children =
       [
         {Registry, name: Scouter.InstanceRegistry, keys: :unique},
-        {Scouter.Instance, main_instance_opts()}
-      ] ++ maybe_enable_instance_manager()
+      ] ++ maybe_main_instance() ++ maybe_enable_instance_manager()
 
     Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  defp maybe_main_instance() do
+    socket = System.get_env("TELEMETRY_SOCKET")
+    port = System.get_env("TELEMETRY_PORT")
+    instance_manager = System.get_env("INSTANCE_MANAGER_SOCKET")
+
+    cond do
+      !socket && !port && !instance_manager ->
+        raise "set TELEMETRY_PORT or TELEMETRY_SOCKET"
+      !socket && !port ->
+        Logger.info("main instance not configured")
+        []
+      true ->
+        [{Scouter.Instance, main_instance_opts()}]
+    end
   end
 
   defp main_instance_opts do
