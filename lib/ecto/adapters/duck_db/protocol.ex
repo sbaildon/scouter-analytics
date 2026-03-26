a43d7e99afbc0a41644c2df09ae7602eb313fa5b
Sean Baildon <sean@baildon.co>
2026-03-26 11:38:58 -0300

    automatic migration for ducklake

--- lib/ecto/adapters/duck_db/protocol.ex
+++ lib/ecto/adapters/duck_db/protocol.ex
@@ -21,7 +21,7 @@ defmodule Ecto.Adapters.DuckDB.Protocol do
          {:ok, _} <-
            Adbc.Connection.query(
              conn,
-             "ATTACH IF NOT EXISTS 'ducklake:sqlite:#{Scouter.Instance.lakehouse_catalog_path(instance)}' AS ducklake (DATA_PATH '#{Scouter.Instance.lakehouse_data_path(instance)}');"
+             "ATTACH IF NOT EXISTS 'ducklake:sqlite:#{Scouter.Instance.lakehouse_catalog_path(instance)}' AS ducklake (DATA_PATH '#{Scouter.Instance.lakehouse_data_path(instance)}', AUTOMATIC_MIGRATION true);"
            ),
          {:ok, _} <- Adbc.Connection.query(conn, "USE ducklake;"),
          {:ok, _} <- Adbc.Connection.query(conn, "CALL ducklake.set_option('parquet_compression', 'zstd');"),
